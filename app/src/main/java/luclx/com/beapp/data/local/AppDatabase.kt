00caf248a50525f5740203e93db9ac1cd4f153e2
Luc LX <luc.le@be.xyz>
2019-07-31 18:21:18 +0700

    tip for room

--- app/src/main/java/luclx/com/beapp/data/local/AppDatabase.kt
+++ app/src/main/java/luclx/com/beapp/data/local/AppDatabase.kt
@@ -5,6 +5,9 @@ import androidx.room.RoomDatabase
 import luclx.com.beapp.data.local.dao.CardDao
 import luclx.com.beapp.data.local.entity.CardEntity
 
+/**
+ * https://medium.com/androiddevelopers/7-pro-tips-for-room-fbadea4bfbd1#4785
+ */
 @Database(entities = [CardEntity::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun cardDao(): CardDao
